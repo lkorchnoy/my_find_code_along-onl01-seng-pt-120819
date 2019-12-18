18cc5ccd1973713bb33bf1d3031342813f2b1a27
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-18 06:40:23 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,41 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    i = i + 1
+  end
 end
+
+
+
+def my_find(collection)
+   i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+end
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
+  end
+end
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
+end
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
